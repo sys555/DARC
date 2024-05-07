1978a6bf0aef3d0311ebcf27d92401bfb6d90f88
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2024-05-07 10:56:44 +0800

    fix ci

--- llmsafetychallenge/core/actor.py
+++ llmsafetychallenge/core/actor.py
@@ -5,13 +5,12 @@ from typing import Dict
 class AbstractActor(metaclass=ABCMeta):
     def __init__(self):
         self._address_book: Dict[str, str] = dict()
-    
-    def recv():
+
+    def recv(self):
         raise NotImplementedError
-    
-    def send():
+
+    def send(self):
         raise NotImplementedError
-    
-    def spawn_new_actor():
+
+    def spawn_new_actor(self):
         raise NotImplementedError
-    
