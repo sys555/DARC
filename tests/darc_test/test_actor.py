8bfec5e5d423e08e4686acb03fabc08ea0bf9135
banana <sys555@outlook.com>
2024-05-08 20:49:04 +0800

    add note

--- tests/darc_test/test_actor.py
+++ tests/darc_test/test_actor.py
@@ -9,6 +9,7 @@ class Actor(AbstractActor):
     def __init__(self, name):
         super().__init__()
         self.name = name
+        # 测试跟踪
         self.received_messages = []  # Add this to track received messages
         
     def register_actor(self, actor):
@@ -17,6 +18,7 @@ class Actor(AbstractActor):
         self._instance[actor.actor_ref.actor_urn] = actor.actor_ref
 
     def on_receive(self, message):
+        # 测试跟踪
         self.received_messages.append(message)  # Log each received message
         if message.to_agent in self._address_book:
             self.send(self._instance[self._address_book[message.to_agent]], message)
@@ -36,7 +38,8 @@ class TestActorCommunication:
         
         # Return proxies for communication testing
         yield alice.proxy(), bob.proxy()
-        
+    
+    ## 通过 send 启动通信，通过验证属性中的 received_messages 消息数量，验证 alice 和 bob 间的通信逻辑可行
     def test_actors_can_communicate(self, alice_and_bob):
         alice, bob = alice_and_bob
 
