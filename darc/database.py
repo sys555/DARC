cb3125ed03eca80d906d0e15727d5f3b344cea7d
banana <sys555@outlook.com>
2024-05-15 14:27:04 +0800

    还原 attacker, evaluator

--- darc/database.py
+++ darc/database.py
@@ -1,1 +1,12 @@
+from darc.darc.node import Node
 
+
+class DatasetDB(Node):
+    def __init__(self, db: str) -> None:
+        super().__init__()
+        self.db: str = db
+
+    @Node.process(["Attacker:DB"])
+    def handle_query(self, query: str) -> str:
+        print(f"Handling query in {self.db}: {query}")
+        return f"Query results for {query} from {self.db}"
