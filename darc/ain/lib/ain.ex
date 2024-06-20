261d9b2265b686db8b7944adc11969604d981357
banana <sys555@outlook.com>
2024-06-20 17:11:35 +0800

    1. load node with json; 2. edit gitignore

---
+++ darc/ain/lib/ain.ex
@@ -0,0 +1,18 @@
+defmodule Ain do
+  @moduledoc """
+  Documentation for `Ain`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ain.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
