6c303163aeda70b191787641bafb8042515cee72
banana <sys555@outlook.com>
2024-06-20 18:02:52 +0800

    send_logs_to_other_node

--- darc/ain/lib/actor_model_server.ex
+++ darc/ain/lib/actor_model_server.ex
@@ -1,23 +1,55 @@
 defmodule Ain.ActorModelServer do
   use GenServer
 
-  # 启动 GenServer
+  # 启动 GenServer，使用 init 字段作为名称注册
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+    name = String.to_atom(args["init"])  # 将 init 字段转换为原子作为名称
+    GenServer.start_link(__MODULE__, args, name: name)
   end
 
   # 初始化
   def init(args) do
-    state = %{init: args["init"], env: args["env"], logs: args["logs"]}
+    state = %{
+      init: args["init"],
+      env: args["env"],
+      logs: args["logs"]
+    }
     {:ok, state}
   end
 
-  # 打印日志消息
-  def print_logs(pid) do
-    GenServer.call(pid, :print_logs)
+  # 发送当前节点的日志到另一个指定节点
+  def send_logs_to_other_node(source_name, target_name) do
+    # 使用 case 语句来处理所有可能的返回
+    case GenServer.call(String.to_atom(source_name), :get_logs) do
+      logs when is_list(logs) ->
+        GenServer.cast(String.to_atom(target_name), {:receive_logs_from_other, logs})
+
+      {:ok, logs} when is_list(logs) ->
+        GenServer.cast(String.to_atom(target_name), {:receive_logs_from_other, logs})
+
+      _ ->
+        IO.puts("Failed to retrieve logs from #{source_name}")
+    end
   end
 
-  # 处理调用
+  # 从其他节点接收日志并打印
+  def handle_cast({:receive_logs_from_other, logs}, state) do
+    IO.puts("Received logs from another node:")
+    Enum.each(logs, &IO.puts/1)
+    {:noreply, state}
+  end
+
+  # 获取当前节点的日志
+  def handle_call(:get_logs, _from, state) do
+    {:reply, state.logs, state}
+  end
+
+  # 打印日志消息
+  def print_logs(name) do
+    GenServer.call(String.to_atom(name), :print_logs)
+  end
+
+  # 处理来自其他节点的打印请求
   def handle_call(:print_logs, _from, state) do
     Enum.each(state.logs, &IO.puts/1)
     {:reply, :ok, state}
---
+++ darc/ain/lib/examples/2_communicate.ex
@@ -0,0 +1,26 @@
+defmodule Communicate do
+  def run do
+    # 配置并启动第一个节点，节点名为 :node1
+    {:ok, pid1} = Ain.ActorModelServer.start_link(%{
+      "init" => "node1",
+      "env" => "compute_prefix",
+      "logs" => ["Node1 log message 1", "Node1 log message 2"]
+    })
+
+    # 配置并启动第二个节点，节点名为 :node2
+    {:ok, pid2} = Ain.ActorModelServer.start_link(%{
+      "init" => "node2",
+      "env" => "compute_prefix",
+      "logs" => ["Node2 log message 1", "Node2 log message 2"]
+    })
+
+    # 等待一小段时间确保两个节点都已启动并注册
+    :timer.sleep(1000)
+
+    # 从节点 :node1 发送日志到节点 :node2
+    Ain.ActorModelServer.send_logs_to_other_node("node1", "node2")
+
+    # 从节点 :node2 打印其接收到的日志
+    Ain.ActorModelServer.print_logs("node2")
+  end
+end
