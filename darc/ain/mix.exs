3f6b57443525129d2ff46c391c6e94ca20ceadc6
banana <sys555@outlook.com>
2024-06-25 16:37:23 +0800

    +P 1000k

--- darc/ain/mix.exs
+++ darc/ain/mix.exs
@@ -7,7 +7,8 @@ defmodule Ain.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixir_opts: [erl_opts: "--erl \"-args_file ./vm.args\""]
     ]
   end
 
